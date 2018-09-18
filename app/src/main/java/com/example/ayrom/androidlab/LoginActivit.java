ba25bdd5df4f7c137dded4bfbd4ea0013a97b9c6
ayrom <ayrom>
2018-09-18 10:40:22 -0400

    initial commit

---
+++ app/src/main/java/com/example/ayrom/androidlab/LoginActivit.java
@@ -0,0 +1,13 @@
+package com.example.ayrom.androidlab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivit extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
